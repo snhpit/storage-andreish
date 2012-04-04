e73e01a226fb52b17dac87c9fd3403575fb47f6b
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-04-04 14:37:00 +0000

    uploaded new files

---
+++ trunk/Scripts/puzzle.js
@@ -0,0 +1,14 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: Andrei_Shostsik
+ * Date: 4/3/12
+ * Time: 9:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+function createTable(){}
+function createSquares(){}
+function mixElements(){}
+function changeSquarePosition(){}
+function compareValue(){}
+
+
