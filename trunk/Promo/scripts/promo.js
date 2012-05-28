090761f89f5e6181af4faf78e86d8e2b12d33206
Andrei.Shostik@gmail.com <Andrei.Shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-05-28 16:45:53 +0000

    updated changes

--- trunk/Promo/scripts/promo.js
+++ trunk/Promo/scripts/promo.js
@@ -1,8 +1,12 @@
 (function($){
     var counter = 0;
     var canSlide = true;
+    var data = null;
+    var numberOfSlides = 0;
 
-    $.fn.slide = function(data, numberOfSlides, time) {
+    $.fn.slide = function(data, time) {
+        data = data;
+        numberOfSlides = data.length;
         addTempElement();
         slide(numberOfSlides);
     };
@@ -17,10 +21,42 @@
     });
 
     $('.slideshow-wrap').mouseover(function(){
-        $('.nav-play').css('opacity', '0.4');
+        $('.nav-play').css('opacity', '0.6');
     }).mouseout(function(){
             $('.nav-play').css('opacity', '0');
         });
+
+    $('.nav-play').live('click', function(){
+        if (canSlide) { return; }
+        $('.nav-right').click();
+        canSlide = true;
+        setTimeout(function() { slide(7) }, 1000);
+    });
+
+    $('.nav-right').live('click', function(){
+        canSlide = false;
+        counter++;
+        var slideShow = $("ul.slideshow");
+        slideShow.animate({left: -parseInt(slideShow.children().first().css('width')) * counter + 'px'}, 1000);
+        if (counter === numberOfSlides) {
+            counter = 0;
+            slideShow.animate({left: '0px'}, 0);
+        }
+        hoverCurrentButton(counter);
+    });
+
+    $('.nav-left').live('click', function(){
+        canSlide = false;
+        var slideShow = $("ul.slideshow");
+        if (counter === 0) {
+            counter = numberOfSlides;
+            slideShow.animate({left: -parseInt(slideShow.css('width')) + 920 + 'px'}, 0);
+        }
+        counter--;
+        slideShow.animate({left: -parseInt(slideShow.children().first().css('width')) * counter + 'px'}, 1000);
+        hoverCurrentButton(counter);
+    });
+
     /**
      * adding first element to the end of collection of slides for cyclic animation
      */
@@ -29,24 +65,27 @@
         slideshow.append(slideshow.children().first().clone());
     }
 
+    /**
+     * navigation buttons
+     */
     function hoverCurrentButton(index) {
         var liCollection = $('ul.slideshow-buttons li');
-        liCollection.eq(index-1).removeClass('current');
+        liCollection.parent().find('.current').removeClass('current');
         liCollection.eq(index).addClass('current');
     }
 
+    // 4 times repeated $("ul.slideshow") may be declare a var?
     function slide(numberOfSlides) {
         hoverCurrentButton(counter);
         setTimeout(function (){
             if (!canSlide) { return; }
             counter++;
             var slideShow =  $("ul.slideshow");
-            slideShow.animate({left: -parseInt(slideShow.children().first().css('width')) * counter + 'px'}, 1000); // not good
-            //counter = counter === numberOfSlides ? 0 : counter;
+            slideShow.animate({left: -parseInt(slideShow.children().first().css('width')) * counter + 'px'}, 1000); // not good, yep?
             if (counter === numberOfSlides) {
                 counter = 0;
                 slideShow.animate({left: '0px'}, 0);
-                //$("ul.slideshow").css('left', '0');
+                //$("ul.slideshow").css('left', '0'); // why it doesn't work?
             }
             slide(numberOfSlides);
         }, 4000);
