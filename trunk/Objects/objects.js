15d75d3be128f5fa2545516f97953e72783c4382
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-05-30 16:44:10 +0000

    some files deleted

---
+++ trunk/Objects/objects.js
@@ -0,0 +1,90 @@
+(function($){
+
+    function Room(){
+        if (!(this instanceof Room)) {
+            return new Room();
+        }
+        if (typeof Room.counter == 'undefined') {
+            Room.counter = 0;
+        }
+
+        Room.counter = Room.counter + 1;
+    }
+
+    var p1 = new Room;
+    console.log(Room.counter);
+    var p2 = new Room();
+    console.log(Room.counter);
+    var p3 = Room;
+    console.log(Room.counter);  // why 2, not 3 ?
+
+
+
+
+
+
+    var Person = function(name) {
+        this.name = name;
+    };
+
+    Person.hi = function(strangerName) {
+        return "Hi, " + strangerName;
+    };
+
+    Person.age = 19;
+
+    console.log(Person.hi("Mahmed"));
+    console.log(Person.age);
+
+    Person.prototype.say = function(){
+        return "I'm " + this.name;
+    };
+
+    var newPerson = new Person("Volodia cho");
+    //console.log(newPerson.hi("Gogi"));  //isn't a func
+    //console.log(newPerson.age); //undefined
+    console.log(newPerson.say());
+
+    for(var i in Person) {
+        if (Person.hasOwnProperty(i)) {
+            console.log("own property of class Person " + i);
+        }
+    }
+
+    for(var i1 in newPerson) {
+        if (newPerson.hasOwnProperty(i1)) {
+            console.log("own property of instance of Person " + i1);
+        }
+    }
+
+
+
+
+
+
+
+    var someObject = {
+        name : "someObject",
+        array: [],
+        add: function(parameter) {
+            this.array.push(parameter);
+        },
+        getName : function() {
+            return this.name;
+        }
+    };
+
+    var personObject = {
+        hands : 2,
+        legs : 2,
+        head : 1
+    };
+
+    var extendObjectTestAdded = $.extend(true, {}, someObject);
+    extendObjectTestAdded.add('test');
+
+    console.log(extendObjectTestAdded.getName());
+    console.log(extendObjectTestAdded);
+    console.log(someObject);
+
+})(jQuery); // jQuery not determined
