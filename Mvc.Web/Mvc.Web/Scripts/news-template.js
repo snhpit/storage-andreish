b8e122dbae287d4920122330363bfeac7406241d
Andrei.Shostik <andrei.shostik@gmail.com>
2012-06-05 17:11:29 +0000

    new project commited

---
+++ Mvc.Web/Mvc.Web/Scripts/news-template.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var smiles = [
+                { title: "Awesome", picture: "120px-Awesome.png", info: "fish", whichClass: 'first' },
+                { title: "Awosome", picture: "120px-Awosome.png", info: "fish" },
+                { title: "Eyebrow", picture: "120px-Eyebrow.jpg", info: "fish" },
+                { title: "HowdoiAwesomeanswer", picture: "120px-HowdoiAwesomeanswer.png", info: "fish" },
+                { title: "Shocksmilie", picture: "120px-Shocksmilie.png", info: "fish" },
+                { title: "Squaresmilie", picture: "120px-Squaresmilie.gif", info: "fish" },
+                { title: "Tardsmilie", picture: "120px-Tardsmilie.jpg", info: "fish", whichClass: 'last' }
+            ];
+
+    $("#articleTemplate").tmpl(smiles).appendTo("#news");
+});
---
+++ Mvc.Web/Mvc.Web/Scripts/web-site.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var cache = location.hash;
+    //        .split(/#%0A/g).join(' ');
+    //        .split(/\<.*\>/g).join();
+    if (cache) {
+        var container = $('#news-section .container');
+        container.html('');
+        container.append(cache);
+    }
+    $('#smiles').click(function () {
+        $('#news-section .container').load('smiles.htm', function (response, status, xhr) {
+            if (status == "error") {
+                var msg = "Sorry but there was an error: ";
+                alert(msg + xhr.status + " " + xhr.statusText + '. Please check your connection or wait a little bit.');
+            }
+            if (status == "success") {
+                setTimeout(function () {
+                    location.hash = $('#news').html();
+                }, 100);
+            }
+        });
+    });
+    $('.close').live("click", function () {
+        $(this).parent().parent().parent().remove();
+        location.hash = $('#news').html();
+    });
+    $(window).unload(function () {
+        location.hash = $('#news').html();
+    });
+});
