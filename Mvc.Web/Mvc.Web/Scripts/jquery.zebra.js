455f66aa4bacb20fc18c31940614a079c856a22f
Andrei.Shostik <andrei.shostik@gmail.com>
2012-06-06 12:29:01 +0000

    changes commited

--- Mvc.Web/Mvc.Web/Scripts/jquery.zebra.js
+++ Mvc.Web/Mvc.Web/Scripts/jquery.zebra.js
@@ -1,44 +1,42 @@
-jQuery.fn.zebra = function(options){
-    
-	var options = jQuery.extend({
-		bgEven: '#FFC080', // бэкграунд для четных строк
-        bgOdd: '#FFDFBF', // бэкграунд для нечетных строк
-        fontEven: '#AA7239', // цвет шрифта четных строк
-        fontOdd: '#AA7239', // цвет шрифта нечетных строк
-        bgHover: '#FF8000', // бэкграунд при hover
-        fontHover: '#55391C' // цвет шрифта при hover
-	}, options);
-	
-	return this.each(function() {
+(function ($) {
+    $.fn.zebra = function (options) {
+        var options = $.extend({
+            bgEven: '#FFC080',
+            bgOdd: '#FFDFBF',
+            fontEven: '#AA7239',
+            fontOdd: '#AA7239',
+            bgHover: '#FF8000',
+            fontHover: '#55391C'
+        }, options);
 
-		jQuery(this).find('tr:even')
-		            .css('background-color', options.bgEven)
-		            .css('color', options.fontEven)
-		            .hover(
-		                function () {
-		                	jQuery(this).css('background-color', options.bgHover)
-		                	       .css('color', options.fontHover);
-		                }, 
-                        function () {
-                        	jQuery(this).css('background-color', options.bgEven)
-                        	       .css('color', options.fontEven);
-                        }
+        return this.each(function () {
+            $(this).find('tr:even')
+                .css('background-color', options.bgEven)
+                .css('color', options.fontEven)
+                .hover(
+                function () {
+                    $(this).css('background-color', options.bgHover)
+                        .css('color', options.fontHover);
+                },
+                function () {
+                    $(this).css('background-color', options.bgEven)
+                        .css('color', options.fontEven);
+                }
 		            );
-		
-        jQuery(this).find('tr:odd')
-                    .css('background-color', options.bgOdd)
-                    .css('color', options.fontOdd)
-                    .hover(
-                        function () {
-                        	jQuery(this).css('background-color', options.bgHover)
-                        	       .css('color', options.fontHover);
-                        }, 
-                        function () {
-                        	jQuery(this).css('background-color', options.bgOdd)
-                        	       .css('color', options.fontOdd);
-                        }
-                    );
-        
-	});
 
-};
+            $(this).find('tr:odd')
+                .css('background-color', options.bgOdd)
+                .css('color', options.fontOdd)
+                .hover(
+                function () {
+                    $(this).css('background-color', options.bgHover)
+                        .css('color', options.fontHover);
+                },
+                function () {
+                    $(this).css('background-color', options.bgOdd)
+                        .css('color', options.fontOdd);
+                }
+                    );
+        });
+    };
+})(jQuery);
--- Mvc.Web/Mvc.Web/Scripts/web-site.js
+++ Mvc.Web/Mvc.Web/Scripts/web-site.js
@@ -1,12 +1,39 @@
-﻿$(document).ready(function () {
-    var cache = location.hash;
-    //        .split(/#%0A/g).join(' ');
-    //        .split(/\<.*\>/g).join();
-    if (cache) {
-        var container = $('#news-section .container');
-        container.html('');
-        container.append(cache);
-    }
+﻿$(function ($) {
+    //    var cache = location.hash;
+    //    //        .split(/#%0A/g).join(' ');
+    //    //        .split(/\<.*\>/g).join();
+    //    if (cache) {
+    //        var container = $('#news-section .container');
+    //        container.html('');
+    //        container.append(cache);
+    //    }
+
+    var dates = $("#from, #to").datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3,
+        onSelect: function (selectedDate) {
+            var option = this.id == "from" ? "minDate" : "maxDate",
+                        instance = $(this).data("datepicker"),
+                        date = $.datepicker.parseDate(
+                            instance.settings.dateFormat ||
+                                $.datepicker._defaults.dateFormat,
+                            selectedDate, instance.settings);
+            dates.not(this).datepicker("option", option, date);
+        }
+    });
+
+    $('table.standart').zebra();
+
+    $('table.changed-options').zebra({
+        bgEven: '#CC66CC',
+        bgOdd: '#E6ACE6',
+        fontEven: '#662266',
+        fontOdd: '#662266',
+        bgHover: '#331133',
+        fontHover: '#FFFFFF'
+    });
+
     $('#smiles').click(function () {
         $('#news-section .container').load('smiles.htm', function (response, status, xhr) {
             if (status == "error") {
@@ -15,16 +42,16 @@
             }
             if (status == "success") {
                 setTimeout(function () {
-                    location.hash = $('#news').html();
+                    //location.hash = $('#news').html();
                 }, 100);
             }
         });
     });
     $('.close').live("click", function () {
         $(this).parent().parent().parent().remove();
-        location.hash = $('#news').html();
+        //location.hash = $('#news').html();
     });
     $(window).unload(function () {
-        location.hash = $('#news').html();
+        //location.hash = $('#news').html();
     });
-});
+})(jQuery);
