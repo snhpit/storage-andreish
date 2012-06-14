9007551ff60bb5f23e605fadd6a74b0d86a445fb
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-06-14 09:17:38 +0000

    tableSorter finded todo: make tablePaging

--- trunk/Mvc.Web/Mvc.Web/Providers/ProviderFactory.cs
+++ trunk/Mvc.Web/Mvc.Web/Providers/ProviderFactory.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Parameters;
+using Ninject.Syntax;
+using Ninject.Web.Mvc;
 
 namespace Mvc.Web.Providers
 {
     public class ProviderFactory : IProviderFactory
     {
+        private readonly NinjectDependencyResolver _dependencyResolver;
+        public ProviderFactory(IResolutionRoot resolutionRoot)
+        {
+            _dependencyResolver = new NinjectDependencyResolver(resolutionRoot);
+        }
+
         IProvider IProviderFactory.Create(string providerName)
         {
+            if (providerName == null) { return null; }
             if (providerName.ToLower() == typeof(GoogleProvider).Name.ToLower())
             {
-                return new GoogleProvider();
+                return _dependencyResolver.GetService(typeof(GoogleProvider)) as GoogleProvider;
             }
             if (providerName.ToLower() == typeof(YahooProvider).Name.ToLower())
             {
-                return new YahooProvider();
+                return _dependencyResolver.GetService(typeof(YahooProvider)) as YahooProvider;
             }
             return null;
         }
