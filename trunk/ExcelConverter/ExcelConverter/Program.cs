7d3a62f2931b821aa749c00573d98d0151850bbf
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-06-20 16:11:33 +0000

    converter commited

---
+++ trunk/ExcelConverter/ExcelConverter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new DropBoxProvider();
+            //var stream = provider.GetStreamData();
+            //provider.GetFromIExcel();
+            provider.GetFromEePlus();
+        }
+    }
+}
