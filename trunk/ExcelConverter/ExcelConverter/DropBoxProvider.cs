374a096d41ddbd72f31c5d1d96c06c4889273a4a
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-06-25 09:11:00 +0000

    GetDataFromEePlus method added GetDataFromExcelInterop method added

--- trunk/ExcelConverter/ExcelConverter/DropBoxProvider.cs
+++ trunk/ExcelConverter/ExcelConverter/DropBoxProvider.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Xml.Linq;
 using Excel;
 using OfficeOpenXml;
+using Microsoft.Office.Interop.Excel;
 
 namespace ExcelConverter
 {
@@ -18,17 +19,17 @@ namespace ExcelConverter
     {
         private string url = "https://dl.dropbox.com/u/34287000/Complex%20version%2018.xlsx";
         private string file = "Complex version 18.xlsx";
-        private string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"));
+        private readonly string _path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"));
 
         public void GetFromIExcel()
         {
-            using (FileStream stream = File.Open(Path.Combine(path, file), FileMode.Open, FileAccess.Read))
+            using (FileStream stream = File.Open(Path.Combine(_path, file), FileMode.Open, FileAccess.Read))
             {
                 using (IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream))
                 {
                     // попробовать сохранить поток, а потом открыть CreateBinaryReader, возможно проблемы с AsDataSet
 
-                    excelReader.IsFirstRowAsColumnNames = true;
+                    //excelReader.IsFirstRowAsColumnNames = true;
                     DataSet result = excelReader.AsDataSet();
 
                     var value = result.Tables[0].Rows[0];
@@ -45,22 +46,89 @@ namespace ExcelConverter
             }
         }
 
-        public void GetFromEePlus()
+        public void GetDataFromExcelInterop()
         {
-            using (FileStream stream = File.Open(Path.Combine(path, file), FileMode.Open, FileAccess.Read))
+            string Path = _path + file;
+            
+            Application app = new Application();
+            //Excel.Worksheet NwSheet;
+            Range ShtRange;
+            // create the workbook object by opening  the excel file.
+            Workbook workBook = app.Workbooks.Open(Path, 0, true, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            // Get The Active Worksheet Using Sheet Name Or Active Sheet
+            Worksheet workSheet = (Worksheet)workBook.ActiveSheet;
+            int index = 1;
+            // that is which cell in the excel you are interesting to read.
+            object rowIndex = 1;
+            object colIndex1 = 1;
+            object colIndex2 = 2;
+            object colIndex3 = 3;
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                while (((Range)workSheet.Cells[rowIndex, colIndex1]).Value2 != null)
+                {
+                    rowIndex = index;
+                    string firstName = Convert.ToString(((Range)workSheet.Cells[rowIndex, colIndex1]).Value2);
+                    string lastName = Convert.ToString(((Range)workSheet.Cells[rowIndex, colIndex2]).Value2);
+                    string Name = Convert.ToString(((Range)workSheet.Cells[rowIndex, colIndex3]).Value2);
+                    string line = firstName + "," + lastName + "," + Name;
+                    sb.Append(line); sb.Append(Environment.NewLine);
+                    Console.WriteLine(" {0},{1},{2} ", firstName, lastName, Name);
+                    index++;
+                }
+
+                //Writetofile(sb.ToString());
+
+                ShtRange = workSheet.UsedRange;
+                Object[,] s = ShtRange.Value;
+
+
+            }
+            catch (Exception ex)
+            {
+                app.Quit();
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+
+        
+        public void GetDataFromExcelLibrary()
+        {
+           
+
+        }
+
+        public List<Tuple<string, List<Tuple<string, object>>>> GetDataFromEePlus()
+        {
+            var excelObjectData = new List<Tuple<string, List<Tuple<string, object>>>>();
+
+            using (FileStream stream = File.Open(Path.Combine(_path, file), FileMode.Open, FileAccess.Read))
             {
                 using (ExcelPackage excelReader = new ExcelPackage(stream))
                 {
-                    var value = excelReader.Workbook.Worksheets.First().Cells.Value;
-                    var firstWorkSheet = excelReader.Workbook.Worksheets.First().Cells.Select(elem => elem.First());
-                    foreach (var item in firstWorkSheet)
+                    foreach (var workSheet in excelReader.Workbook.Worksheets)
                     {
-                        Console.WriteLine(item.Text);
+                        var listOfColumns = new List<Tuple<string, object>>();
+
+                        for (int i = 1; i <= workSheet.Dimension.End.Column; i++)
+                        {
+                            for (int j = 2; j <= workSheet.Dimension.End.Row; j++)
+                            {
+                                listOfColumns.Add(new Tuple<string, object>(workSheet.Cells[1, i].Text, workSheet.Cells[j, i].Text));
+                                if (workSheet.Cells[j + 1, i].Value == null) { break; }
+                            }
+                            if (workSheet.Cells[1, i + 1].Value == null) { break; }
+                        }
+
+                        excelObjectData.Add(new Tuple<string, List<Tuple<string, object>>>(workSheet.Name, listOfColumns));
                     }
-                      
-                    Console.WriteLine();
                 }
             }
+            return excelObjectData;
         }
 
         public string GetStreamData()
--- trunk/ExcelConverter/ExcelConverter/Program.cs
+++ trunk/ExcelConverter/ExcelConverter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -7,12 +8,31 @@ namespace ExcelConverter
 {
     class Program
     {
+        private static readonly string Path = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"));
+
         static void Main(string[] args)
         {
             var provider = new DropBoxProvider();
             //var stream = provider.GetStreamData();
             //provider.GetFromIExcel();
-            provider.GetFromEePlus();
+
+            //provider.GetDataFromExcelInterop();
+            
+            //var excelObjectData = provider.GetDataFromEePlus();
+            
+            
+
+            //var r = excelObjectData.Select(elem => new { ListItemName = elem.Item2.Select(el => el.Item1), ListItem = elem.Item2.Select(el => el.Item2) });
+            //foreach (var VARIABLE in r)
+            //{
+            //    for (int i = 0; i < VARIABLE.ListItemName.ToArray().Length; i++)
+            //    {
+            //        File.AppendAllText(Path + "fileEPPExcel.txt", 
+            //            String.Format(@"{0}", VARIABLE.ListItemName.ToArray()[i] + " - " + VARIABLE.ListItem.ToArray()[i] + "\n"));
+            //        //Console.WriteLine("{0}, {1}", VARIABLE.ListItemName.ToArray()[i].ToString(CultureInfo.InvariantCulture), VARIABLE.ListItem.ToArray()[i]);
+            //        //Console.WriteLine("{0}, {1}", VARIABLE.ListItemName.ToArray()[i], VARIABLE.ListItem.ToArray()[i]);
+            //    }
+            //}
         }
     }
 }
