f554f400309ff69c221a30444f30f9f6dbaacb8e
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-07-27 16:13:48 +0000

    console chat uploaded

---
+++ trunk/Chat/Chat/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Fleck;
+
+namespace Chat
+{
+    class Program
+    {
+        private static readonly string Path = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"));
+
+
+        static void Main(string[] args)
+        {
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://localhost:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    var date = SaveMessage(socket.ConnectionInfo.Id, message);
+                    Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send(date + ": " + message));
+                };
+            });
+
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+             
+            Console.ReadKey(true);
+        }
+
+        private static string SaveMessage(Guid id, string message)
+        {
+            var date = DateTime.Now.ToShortTimeString();
+            File.AppendAllText(Path + "chat.txt", id + ": " + date + ": " + message + Environment.NewLine);
+            return date;
+        }
+
+    }
+}
