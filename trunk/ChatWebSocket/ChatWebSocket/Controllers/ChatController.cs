a60cc8eeecfb0bedae15de9b6c2aa667f0c4ec67
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-08-23 15:31:03 +0000

    commited files

---
+++ trunk/ChatWebSocket/ChatWebSocket/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fleck;
+
+namespace ChatWebSocket.Controllers
+{
+    public class ChatController : Controller
+    {  
+        public ActionResult Index()
+        {
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://localhost:8183");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    //Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    //Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    var date = DateTime.Now.ToShortTimeString();
+                    //SaveMessage(socket.ConnectionInfo.Id, message);
+                    //Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send(date + ": " + message));
+                };
+            });
+
+            return View();
+        }
+
+    }
+}
---
+++ trunk/ChatWebSocket/ChatWebSocket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatWebSocket.MvcApplication" Language="C#" %>
