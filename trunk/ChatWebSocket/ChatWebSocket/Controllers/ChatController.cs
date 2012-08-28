9b3771f62589711cceb385cb8275e20379eaba74
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-08-28 12:14:53 +0000

    uploaded many files with changes

--- trunk/ChatWebSocket/ChatWebSocket/Controllers/ChatController.cs
+++ trunk/ChatWebSocket/ChatWebSocket/Controllers/ChatController.cs
@@ -4,35 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Fleck;
+using ChatWebSocket.ChatWcfClient;
+using System.ServiceModel;
 
 namespace ChatWebSocket.Controllers
 {
     public class ChatController : Controller
-    {  
+    {
+        private readonly IChatService _proxy;
+
+        public ChatController(IChatService proxy)
+        {
+            this._proxy = proxy;
+        }
+
         public ActionResult Index()
         {
-            var allSockets = new List<IWebSocketConnection>();
-            var server = new WebSocketServer("ws://localhost:8183");
-            server.Start(socket =>
-            {
-                socket.OnOpen = () =>
-                {
-                    //Console.WriteLine("Open!");
-                    allSockets.Add(socket);
-                };
-                socket.OnClose = () =>
-                {
-                    //Console.WriteLine("Close!");
-                    allSockets.Remove(socket);
-                };
-                socket.OnMessage = message =>
-                {
-                    var date = DateTime.Now.ToShortTimeString();
-                    //SaveMessage(socket.ConnectionInfo.Id, message);
-                    //Console.WriteLine(message);
-                    allSockets.ToList().ForEach(s => s.Send(date + ": " + message));
-                };
-            });
+            ViewBag.Data = _proxy.GetData(5);
+
+            //_proxy.StartServer();
 
             return View();
         }
---
+++ trunk/ChatWebSocket/WcfSocketWebService/ChatWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ChatWebService.asmx.cs" Class="WcfSocketWebService.ChatWebService" %>
