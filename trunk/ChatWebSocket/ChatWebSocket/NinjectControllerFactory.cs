2b79e32c92b36bc2f8e6e77143457f296b44d0ab
andrei.shostik@gmail.com <andrei.shostik@gmail.com@3535fb6a-f193-852c-687c-aeefdfbe3819>
2012-08-29 15:13:15 +0000

    wcf example started

--- trunk/ChatWebSocket/ChatWebSocket/NinjectControllerFactory.cs
+++ trunk/ChatWebSocket/ChatWebSocket/NinjectControllerFactory.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ChatWebSocket.ChatWcfService;
 using Ninject;
 using ChatWebSocket.Controllers;
-using ChatWebSocket.ChatWcfClient;
 using Ninject.Syntax;
 using Ninject.Parameters;
 
